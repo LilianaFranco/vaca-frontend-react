9eea46696091c3c35f3475c9263d602de70943ef
LilianaFranco <franco0123@gmail.com>
2024-03-17 18:49:47 -0500

    Create layout folder and move the navbar component inside it. Delete some unnecesary imports in App.

--- src/pages/logIn/LogInPage.jsx
+++ src/pages/logIn/LogInPage.jsx
@@ -1,6 +1,3 @@
-import { Button } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
 import React from "react";
 
 const LogInPage = () => {
