8de70ccc660cee988000acedc85ec0df072df53a
LilianaFranco <franco0123@gmail.com>
2024-05-21 17:09:41 -0500

    Fix the content type for user and group instance

--- src/services/userServices/UserInstance.js
+++ src/services/userServices/UserInstance.js
@@ -4,6 +4,6 @@ export const UserInstance = axios.create({
   baseURL: "http://localhost:3001/users",
   headers: {
     Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-    "Content-Type": "application json",
+    "Content-Type": "application/json",
   },
 });
