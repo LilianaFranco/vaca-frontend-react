66c3c3bf61f052ee1587329bc82560a6520bf852
LilianaFranco <franco0123@gmail.com>
2024-05-28 14:17:13 -0500

    Delete axios instance

--- src/services/authServices/loginService.js
+++ src/services/authServices/loginService.js
@@ -1,5 +1,14 @@
-import { LogInInstance } from "./loginInstance";
+import axios from "axios";
+
+const axiosConnection = () =>
+  axios.create({
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/login`,
+    //   headers: {
+    //     "Authorization barer": "ASDsadasd122312DXASDasd",
+    //     "Content-Type": "aplication json",
+    //   },
+  });
 
 export const login = async (data) => {
-  return LogInInstance.post("/", data);
+  return axiosConnection().post("/", data);
 };
--- src/services/groupServices/GroupService.js
+++ src/services/groupServices/GroupService.js
@@ -1,19 +1,28 @@
-import { GroupInstance } from "src/services/groupServices/GroupsInstance";
+import axios from "axios";
+
+const axiosConnection = () =>
+  axios.create({
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/groups`,
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  });
 
 export const get = async () => {
-  let groups = GroupInstance.get();
+  let groups = axiosConnection().get();
   return groups;
 };
 
 export const getById = async (id) => {
-  let group = await GroupInstance.get(`/${id}`);
+  let group = await axiosConnection().get(`/${id}`);
   return group;
 };
 
 export const deleteById = async (id) => {
-  return await GroupInstance.delete(`/${id}`);
+  return await axiosConnection().delete(`/${id}`);
 };
 
 export const create = async (data) => {
-  return await GroupInstance.post("/", data);
+  return await axiosConnection().post("/", data);
 };
